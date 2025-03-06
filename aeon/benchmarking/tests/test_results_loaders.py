4849cd0883b78cad892cf11a97d8f389c174ce3e
syncpr-user1 <ewfnewrg@gmail.com>
2025-03-06 05:59:45 -0500

    run_id=7494912669, outcome=failure, bh=ajb/purge, sha=1362ec7143a51e614764de718838ea8e9b005e10

--- aeon/benchmarking/tests/test_results_loaders.py
+++ aeon/benchmarking/tests/test_results_loaders.py
@@ -119,9 +119,6 @@ def test_get_available_estimators():
     classifiers = get_available_estimators(task="classification")
     assert isinstance(classifiers, pd.DataFrame)
     assert classifiers.isin(["HC2"]).any().any()
-    regressors = get_available_estimators(task="regression")
-    assert isinstance(regressors, pd.DataFrame)
-    assert regressors.isin(["DrCIF"]).any().any()
 
 
 @pytest.mark.skipif(
